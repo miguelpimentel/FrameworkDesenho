0ff8f8bdd8fbda1681e239cbd1a358ab74874fce
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2017-06-21 23:29:37 -0300

    Created abstract classes of product and payment

---
+++ lib/e4commerce/product.rb
@@ -0,0 +1,20 @@
+require "abstraction"
+require "active_record"
+
+# class E4CProduct < ActiveRecord::Base -> Rails project, need database
+class E4CProduct
+	abstract
+
+	attr_accessor :title
+	attr_accessor :serial_number
+	attr_accessor :brand
+	attr_accessor :unit_price
+
+	def initialize(title, serial_number, brand, unit_price)
+		@title = title
+		@serial_number = serial_number
+		@brand = brand
+		@unit_price = unit_price
+	end
+
+end
