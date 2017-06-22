51062a2b3683452249c2d7ec440ba2c991e4d878
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2017-06-21 23:29:37 -0300

    Created abstract classes of product and payment

--- lib/e4commerce.rb
+++ lib/e4commerce.rb
@@ -1,11 +1,7 @@
 require "e4commerce/version"
+require "e4commerce/payment.rb"
+require "e4commerce/product.rb"
 
 module E4commerce
 
-	class PagSeguroPayment < ApplicationController
-
-	def test
-		print("Nois")
-	end
-	
 end
---
+++ lib/e4commerce/payment.rb
@@ -0,0 +1,27 @@
+module E4commerce
+
+require "abstraction"
+require "active_record"
+
+	#class E4CPayment < ActiveRecord::Base ~> Rails project, need database
+	class E4CPayment
+		abstract
+
+		attr_accessor :product_list
+		attr_accessor :total
+
+		def calculate_total(products)
+			raise NotImplementedError, "Child must implement method"
+		end
+
+		def create_payment
+			raise NotImplementedError, "Child must implement method"
+		end
+
+		def add_product(product)
+			raise NotImplementedError, "Child must implement method"
+		end
+
+	end
+
+end
