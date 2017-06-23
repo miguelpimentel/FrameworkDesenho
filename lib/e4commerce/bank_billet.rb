ab18d9e1c1ff6dd795fdd0e505e97a1859ec25ce
Harrison <hpedro1195@gmail.com>
2017-06-23 18:53:00 -0300

    Create client class to store client informations and changing the bank billet to be abstract

--- lib/e4commerce/bank_billet.rb
+++ lib/e4commerce/bank_billet.rb
@@ -1,7 +1,7 @@
 module E4commerce
     require "date"
     require "abstraction"
-
+    require "client"
 	class BankBillet < E4CPayment
         absrtact
 		attr_accessor :product_list
@@ -17,29 +17,10 @@ module E4commerce
             return @total
 		end
 
-		def create_payment
-            # Generates and returns bank billet given total price
-            current_time = DateTime.now
-
-            @boleto = Brcobranca::Boleto::BancoBrasil.new :cedente => "João João",
-                :documento_cedente => "12345678912",
-                :sacado => "Maria Maria",
-                :sacado_documento => "12345678900",
-                :valor => @total,
-                :agencia => "4042",
-                :conta_corrente => "61900",
-                :convenio => "1238798",
-                :numero_documento => "7777700168",
-                :data_vencimento => current_time.strftime "%d/%m/%Y",
-                :data_documento => current_time.next_month.strftime "%d/%m/%Y",
-                :instrucao1 => "Pagável na rede bancária até a data de vencimento.",
-                :instrucao2 => "Juros de mora de 2.0% mensal(R$ 0,09 ao dia)",
-                :instrucao3 => "Após vencimento pagável somente nas agências do Banco do Brasil",
-                :instrucao4 => "ACRESCER R$ 4,00 REFERENTE AO BOLETO BANCÁRIO",
-                :sacado_endereco => "Gama, DF"
-
-            send_data @boleto.to_pdf, :filename => "boleto.pdf"
-		end
+		def create_payment(client)
+            
+            raise NotImplementedError, "Child must implement method"
+        end
 
 		def add_product(product)
 			# Adds a product or an array of products to the products list
