88befca05dd84ee9b47b8c857142417ce8e687db
Daniel Moura <danmoura17@gmail.com>
2017-06-22 22:09:57 -0300

    Adding Trasaction history functionality, now is true

--- lib/pagseguro/consults_controller.rb
+++ lib/pagseguro/consults_controller.rb
@@ -24,6 +24,27 @@ class CheckoutController < E4CPayment
   end
 
   def  transaction_history
+
+    report = PagSeguro::Transaction.find_by_date
+
+    while report.next_page?
+      report.next_page!
+      puts "== Page #{report.page}"
+      abort "=> Errors: #{report.errors.join("\n")}" unless report.valid?
+      puts "Report created on #{report.created_at}"
+      puts
+
+      report.transactions.each do |transaction|
+        puts "=> Transaction"
+        puts "   created_at: #{transaction.created_at}"
+        puts "   code: #{transaction.code}"
+        puts "   cancellation_source: #{transaction.cancellation_source}"
+        puts "   payment method: #{transaction.payment_method.type}"
+        puts "   gross amount: #{transaction.gross_amount}"
+        puts "   updated at: #{transaction.updated_at}"
+        puts
+      end
+    end
   end
 
 
