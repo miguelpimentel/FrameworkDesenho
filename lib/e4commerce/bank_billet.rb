8106f164667f26b0ce357c3f15d15635aa3819f0
Felipe <felipe.osorio72@outlook.com.br>
2017-06-22 21:33:52 -0300

    Implement some methods in BankBillet and fix a method in the abstract class

--- lib/e4commerce/bank_billet.rb
+++ lib/e4commerce/bank_billet.rb
@@ -1,19 +1,29 @@
 module E4commerce
 
+    require "abstraction"
+
 	class BankBillet < E4CPayment
+        absrtact
 		attr_accessor :product_list
 		attr_accessor :total
 
-		def calculate_total(products)
+		def calculate_total
+            # Determine total price sum of products in list
 
+            product_list.each do |product|
+                total += product.unit_price
+            end 
+
+            return total
 		end
 
 		def create_payment
-
+            # Generates and returns bank billet given total price
 		end
 
 		def add_product(product)
-
+			# Adds a product or an array of products to the products list
+			product_list << product
 		end
 	end
 
