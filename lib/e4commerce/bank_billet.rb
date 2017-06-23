224f8ef4c2ebfd48aa785362b4da52253cd90c0f
Felipe <felipe.osorio72@outlook.com.br>
2017-06-22 21:19:39 -0300

    Create Bank Billet class inheriting from Payment class

---
+++ lib/e4commerce/bank_billet.rb
@@ -0,0 +1,20 @@
+module E4commerce
+
+	class BankBillet < E4CPayment
+		attr_accessor :product_list
+		attr_accessor :total
+
+		def calculate_total(products)
+
+		end
+
+		def create_payment
+
+		end
+
+		def add_product(product)
+
+		end
+	end
+
+end
